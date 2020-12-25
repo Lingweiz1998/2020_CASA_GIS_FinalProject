b7869709e58be61788b94d839a44be706e38c060
LingweiZHENG <lingwei_zheng@outlook.com>
2020-12-25 11:54:38 +0800

    solve the window issue: projection epsg2263

--- patternanalysis.r
+++ patternanalysis.r
@@ -28,15 +28,6 @@ nysbssData0 <- read_csv(here::here("data", "rawdata","20200917-citibike-tripdata
                        locale = locale(encoding = "latin1"))
 
 
-'''
-LondonBoroughs <- st_read("https://opendata.arcgis.com/datasets/8edafbe3276d4b56aec60991cbddda50_4.geojson")
-BoroughMap <- LondonBoroughs %>%
-  dplyr::filter(str_detect(lad15cd, "^E09"))%>%
-  st_transform(., 27700)
-Harrow <- BoroughMap %>%
-  filter(., lad15nm=="Harrow")
-'''
-
 nycdistricts %>% 
   ggplot() +
   geom_sf(color = "#1F77B4") +
@@ -46,17 +37,14 @@ nycdistricts %>%
   xlab("Longitude") +
   ylab("Latitude")
 
-summary(nysbssData)
+summary(nysbssData0)
 summary(nycdistricts)
 
 
 ##Change CRS if necessary
 CDMap <- nycdistricts %>% 
-  st_transform(.,crs = "WGS84")
+  st_transform(.,crs = "epsg:2263")
 
-## data cleaning
-nysbssData0<- nysbssData %>% slice(723457:776363)
-write.csv(nysbssData0,here::here("data", "rawdata","20200917-citibike-tripdata.csv"), row.names = FALSE)
 
 ## clean name
 colnames(CDMap) <- colnames(CDMap) %>% 
@@ -69,21 +57,23 @@ colnames(nysbssData0) <- colnames(nysbssData0) %>%
   str_replace_all(" ", "_")
 colnames(nysbssData0)
 
+## unique value
+nysbssData0 <- distinct(nysbssData0)
+
+##cut the staten island for better visualization
 CDMap4boro <- CDMap %>%  
   filter(borocd != "501"&borocd != "502"&borocd != "503"&borocd != "595") %>% 
   `colnames<-`(str_to_lower(colnames(CDMap)))
-
-## unique value
-nysbssData0 <- distinct(nysbssData0)
 
 ## st to sf
 nysbssData1 <- st_as_sf(nysbssData0, coords = c("start_station_longitude", "start_station_latitude"), crs = "WGS84")
 nysbssData2 <- st_as_sf(nysbssData0, coords = c("end_station_longitude", "end_station_latitude"),crs = "WGS84")
 
-nysbssData1 <- st_transform(nysbssData1, crs = "WGS84")
-nysbssData2 <- st_transform(nysbssData2, crs = "WGS84")
+nysbssData1 <- nysbssData1 %>% st_transform(.,crs="epsg:2263")
+nysbssData2 <- nysbssData2 %>% st_transform(.,crs="epsg:2263")
 
-summary()
+summary(nysbssData1)
+summary(CDMap4boro)
 
 ##compare data in map
 tmap_mode("view")
@@ -93,7 +83,8 @@ tm_shape(nysbssData1) +
   tm_dots()
 
 #now set a window as the borough boundary
-window <- as.owin(nycbond)
+CDmap4borocb <- st_combine(CDMap4boro)
+window <- as.owin(CDMap4boro)
 plot(window)
 
 summary()
